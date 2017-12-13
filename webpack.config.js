45050b593f89553dec28b7ea1877713622abffdc
jlaskowska <leszek.laskowski@live.com>
2017-12-13 13:22:36 -0500

    API and testing setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,33 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+//const path = require('path');
+
+
 module.exports = {
-    devtool: 'inline-source-map',
-    entry: [
-        'webpack-dev-server/client?http://127.0.0.1:8080/',
-        'webpack/hot/only-dev-server',
-        './src'
-    ],
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
+    //devtool: 'inline-source-map',
+    entry: {
+        //'webpack-dev-server/client?http://127.0.0.1:8080/',
+        //'webpack/hot/only-dev-server',
+        'javascripts/build.js': './src/index.jsx'
     },
-    //resolve: {
+    output: {
+        filename: '[name]',
+        path: path.join(__dirname, 'public')
+    },
+    resolve: {
     //    modulesDirectories: ['node-modules', 'src'],
-    //    extensions: ['', '.js']
-   // },
+          extensions: ['.js', '.jsx'],
+    },
     module: {
         loaders: [
         {
-            test: /\.js?$/,
-            exclude: /(node_modules)/,
+            test: /\.jsx?$/,
+            exclude: /(node_modules|public\/)/,
             loader: 'babel-loader',
             query: {
-                presets: ['react', 'es2015']
+                presets: ['react', 'es2017']
             }
         }    
         ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin()
-    ]
+    }
 
 }
