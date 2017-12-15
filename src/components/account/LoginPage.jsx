4d658fe032f90080288df6656dba4a89de5d45f9
jlaskowska <leszek.laskowski@live.com>
2017-12-14 21:03:56 -0500

    adding loging functions

--- src/components/account/LoginPage.jsx
+++ src/components/account/LoginPage.jsx
@@ -4,7 +4,35 @@ import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 export default class LoginPage extends React.Component {
     constructor(props) {
         super(props);
+
+        // bound functions
+        this.compileFormData = this.compileFormData.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);        
+
+        // component state
+        this.state = {
+          email: '',
+          password: '',
+        };
     }
+
+    // update state as email value changes
+    handleEmailChange(e) {
+      this.setState({ email: e.target.value });
+    }
+  
+    // update state as password value changes
+    handlePasswordChange(e) {
+      this.setState({ password: e.target.value });
+    }
+  
+    compileFormData() {
+      const { loginFunction } = this.props;
+      const formData = this.state;
+      loginFunction(formData);
+    }
+
     render() {
         return (
             <div className="row justify-content-center">
@@ -13,13 +41,25 @@ export default class LoginPage extends React.Component {
               <Form>
                 <FormGroup>
                   <Label for="exampleEmail">Email</Label>
-                  <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                  <Input 
+                    name="email"
+                    id="exampleEmail"
+                    placeholder="noreply@musiclist.com"
+                    value={this.state.email}
+                    onChange={this.handleEmailChange}   
+                  />
                 </FormGroup>
                 <FormGroup>
                   <Label for="examplePassword">Password</Label>
-                  <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+                  <Input 
+                    name="password"
+                    id="examplePassword"
+                    placeholder="password"
+                    value={this.state.password}
+                    onChange={this.handlePasswordChange}
+                  />
                 </FormGroup>
-                <Button>Log In</Button>
+                <Button onClick={this.compileFormData}>Log In</Button>
               </Form>
       
             </div>
