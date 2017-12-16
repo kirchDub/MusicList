bacca133a14a7edd4eb880a70ad5d970cff73324
jlaskowska <leszek.laskowski@live.com>
2017-12-16 10:27:39 -0500

    Registration Page - cont.

--- routes/api/authentication.js
+++ routes/api/authentication.js
@@ -22,21 +22,6 @@ router.get('/logout', (req, res) => {
     return res.send(JSON.stringify(req.user));
 });
 
-//POST to /register
-router.post('/register', (req, res) => {
-    //Create a user object using values from incoming JSON
-    const newUser = new User(req.body);
-    //Save, via Passport Register method, the user
-    User.register(newUser, req.body.password, (err, user) => {
-        // if there is a problem send back JSON with the error
-        if (err) {
-            return res.send(JSON.stringify({error: err}));
-        }
-        //Otherwise, for now, send back a JSON object with the new user's info
-        return res.send(JSON.stringify(user));
-    });
-});
-
 //POST to /login
 router.post('/login', async (req, res) => {
     // look up the user by their email
@@ -51,8 +36,38 @@ router.post('/login', async (req, res) => {
         if (req.user) {
             return res.send(JSON.stringify(req.user));
         }
-        //Otherwise, return an error
-        return res.send(JSON.stringify({error: err}));
+        
+        // Otherwise log them in
+        return passport.authenticate('local')(req, res, () => {
+            // If logged in, we should have user info to send back
+            if (req.user) {
+            return res.send(JSON.stringify(req.user));
+            }
+            // Otherwise return an error
+            return res.send(JSON.stringify({ error: 'There was an error logging in' }));
+        });
+    });
+});
+
+//POST to /register
+router.post('/register', (req, res) => {
+    //Create a user object using values from incoming JSON
+    const newUser = new User(req.body);
+    //Save, via Passport Register method, the user
+    User.register(newUser, req.body.password, (err) => {
+        // if there is a problem send back JSON with the error
+        if (err) {
+            return res.send(JSON.stringify({error: err}));
+        }
+        // Otherwise log them in
+        return passport.authenticate('local')(req, res, () => {
+            // If logged in, we should have user info to send back
+            if (req.user) {
+            return res.send(JSON.stringify(req.user));
+            }
+            // Otherwise return an error
+            return res.send(JSON.stringify({ error: 'There was an error logging in' }));
+        });
     });
 });
 
